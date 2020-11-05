e9aa5964fd53dcf6454a57f0995e65c397465590
Mike Wallio <michael.wallio@outlook.com>
2020-11-05 17:09:23 -0500

    Add more split tests

--- tests/src/string_split.cpp
+++ tests/src/string_split.cpp
@@ -7,7 +7,6 @@ TEST(StringSplit, RawString)
     const wchar_t *str{L"Hello World"};
     std::array<std::wstring_view, 2> output;
     mtl::string::split(str, L" ", std::begin(output));
-
     ASSERT_EQ(output[0], L"Hello");
     ASSERT_EQ(output[1], L"World");
 }
@@ -39,6 +38,17 @@ TEST(StringSplit, OutputBasicString)
 
     // Need to specify output type as template argument if not using string_views
     mtl::string::split<wchar_t, std::wstring>(str, L" ", std::back_inserter(output));
+
+    ASSERT_EQ(output.size(), 2);
+    ASSERT_EQ(output[0], L"Hello");
+    ASSERT_EQ(output[1], L"World");
+}
+
+TEST(StringSplit, LastIsDelim)
+{
+    const wchar_t *str{L"Hello World "};
+    std::vector<std::wstring_view> output;
+    mtl::string::split(str, L" ", std::back_inserter(output));
 
     ASSERT_EQ(output.size(), 2);
     ASSERT_EQ(output[0], L"Hello");
